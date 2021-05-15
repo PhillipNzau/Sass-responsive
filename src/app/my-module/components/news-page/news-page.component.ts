82b8a408b656e656dd8d51b6d1d27e9bc1108eeb
phillip nzau junior <nzau78@gmail.com>
2021-05-15 14:16:34 +0300

    related blog

--- src/app/my-module/components/news-page/news-page.component.ts
+++ src/app/my-module/components/news-page/news-page.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-page.component.sass']
 })
 export class NewsPageComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //function to return list of numbers from 0 to n-1
+  numSequence(n: number): Array<number> {
+    return Array(n);
   }
 
 }
