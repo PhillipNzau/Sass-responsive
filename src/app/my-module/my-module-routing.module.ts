c266aeb68d2ea93ed2c6a99ca2ba85021f1f6253
phillip nzau junior <nzau78@gmail.com>
2021-05-19 10:50:06 +0300

    add-blog page

--- src/app/my-module/my-module-routing.module.ts
+++ src/app/my-module/my-module-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {NewsPageComponent} from './components/news-page/news-page.component';
 import {DetailedNewsComponent} from './components/news-page/children/detailed-news/detailed-news.component';
+import {AddBlogComponent} from './components/news-page/children/add-blog/add-blog.component';
 
 const routes: Routes = [
   {path: 'news', component: NewsPageComponent},
-  {path: 'detail-news', component: DetailedNewsComponent}
+  {path: 'detail-news', component: DetailedNewsComponent},
+  {path: 'add-blog', component: AddBlogComponent}
 ];
 
 @NgModule({
