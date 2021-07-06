f6b5a56f4c060edee491330748cbac4e198ce2ec
phillip nzau junior <nzau78@gmail.com>
2021-07-06 16:33:25 +0300

    upload blog form setup

--- src/app/my-module/components/news-page/children/add-blog/add-blog.component.ts
+++ src/app/my-module/components/news-page/children/add-blog/add-blog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Blog} from "../../../../../interfaces/blogs";
+import {Category} from "../../../../../interfaces/category";
+import {BackendService} from "../../../../services/backend.service";
 
 @Component({
   selector: 'app-add-blog',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-blog.component.sass']
 })
 export class AddBlogComponent implements OnInit {
+  categories: Category[] = [];
+  blog: Blog[] = [];
 
-  constructor() { }
+  constructor(
+    public service: BackendService,
+
+  ) { }
 
   ngOnInit(): void {
+    this.service.getAllCategories()
+      .subscribe(categoryResult => {
+        this.categories = categoryResult.results;
+      });
   }
 
 }
